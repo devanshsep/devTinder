644f9ca12a925ab56b075b3af3b2cddc324e8480
Devansh Singh <devanshsinghrs@gmail.com>
2025-03-21 22:47:03 +0530

    Exploring get post put patch methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,51 @@ const express = require('express')
 
 const app = express()
 
-app.get('/',function(req,res){
+
+//app.use('/hello/hello ',function(req,res){
+//    res.send('hello ji')
+//    })
+//   
+//    app.use('/test',function(req,res){
+//        res.send("hello hello hello")
+//    })
+//    
+//
+//app.use('/',function(req,res){
+//    res.send('hello world')
+//})
+
+
+app.get('/user',function(req,res){
+   res.send('hello ji')
+})
+
+app.post('/user',function(req,res){
+    res.send('Hello hello hello')
+})
+
+
+
+
+
+
+app.patch('/user',function(req,res){
+    res.send('only ji')
+})
+
+app.put('/user',function(req,res){
     res.send('hello world')
 })
 
-app.get('/test',function(req,res){
-res.send('hello ji')
-})
+
+
+
 
 
 app.listen(7000,function(){
     console.log("server is running")
 })
+
+
+
 
