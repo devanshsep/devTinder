f9e40c72274e13a4a61b8ae8a374cf42bda78810
Devansh Singh <devanshsinghrs@gmail.com>
2025-04-19 16:09:16 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,12 +3,57 @@ const express = require("express")
 
 const app = express()
 
-app.use('/',(req,res)=>{
-   res.send("Hello ji")
-})
+// This will Handle only get call to the /user
 
 
+
+
+
+// This will work on all Http methods
+
+app.use('/user',
+[  (req,res,next)=>{
+
+ 
+ // res.send("Hello ji 1")
+  next();
+
+
+},
+(req,res,next)=>{
+//res.send("hello ji 2")
+
+next()
+},
+
+
+
+
+(req,res,next) =>{
+ // res.send("hello ji 3")
+
+  
+  next()
+},
+
+(req,res,next) =>{
+ // res.send("hello ji 4")
+
+  next()
+}],
+
+(req,res) =>{
+  res.send("hello ji 5")
+
+
+}
+
+
+)
 
 app.listen(7000,()=>{
     console.log('Server is running')
+  
 })
+
+
